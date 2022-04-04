4d1288469237c59d824a58eb10335d899bd78687
CemTunna <cemroot@gmail.com>
2022-04-04 23:51:13 +0300

    latest

---
+++ frontend/pages/about.tsx
@@ -0,0 +1,14 @@
+import { Grid, Typography } from '@mui/material';
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <Grid>
+      <Typography variant='h1'>About</Typography>
+      <Typography variant='h5'>This is an app to find latest events</Typography>
+      <Typography variant='h5'>Version 1.0.0</Typography>
+    </Grid>
+  );
+};
+
+export default AboutPage;
