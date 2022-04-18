ac68e22cbec778637a27beefe2c0e14718a9012b
CemTunna <cemroot@gmail.com>
2022-04-18 21:24:12 +0300

    context API config

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,11 +1,14 @@
 import { List, ListItem, Grid } from '@mui/material';
-import React from 'react';
+import React, { useContext } from 'react';
 import styles from 'Styles/Header.module.css';
 import Link from 'next/link';
 import Search from './Search';
 import AddIcon from '@mui/icons-material/Add';
 import LoginIcon from '@mui/icons-material/Login';
+import AuthContext from 'Context/AuthContext';
 const Header = () => {
+  const { user, logout } = useContext(AuthContext);
+  console.log('user', user);
   return (
     <header className={styles.header}>
       <Grid className={styles.logo}>
@@ -17,25 +20,32 @@ const Header = () => {
       <nav>
         <List className={styles.list}>
           <ListItem>
-            <Link href='/events/add'>
-              <a className={styles.regular}>
-                Add Event
-                <AddIcon style={{ color: '#fff', marginLeft: 5 }} />
-              </a>
-            </Link>
-          </ListItem>
-          <ListItem>
             <Link href='/events'>
               <a className={styles.regular}>Events</a>
             </Link>
           </ListItem>
-          <ListItem>
-            <Link href='/account/login'>
-              <a className={styles.login}>
-                Login <LoginIcon style={{ color: '#fff', marginLeft: 5 }} />
-              </a>
-            </Link>
-          </ListItem>
+          {user ? (
+            <>
+              <ListItem>
+                <Link href='/events/add'>
+                  <a className={styles.regular}>
+                    Add Event
+                    <AddIcon style={{ color: '#fff', marginLeft: 5 }} />
+                  </a>
+                </Link>
+              </ListItem>
+            </>
+          ) : (
+            <>
+              <ListItem>
+                <Link href='/account/login'>
+                  <a className={styles.login}>
+                    Login <LoginIcon style={{ color: '#fff', marginLeft: 5 }} />
+                  </a>
+                </Link>
+              </ListItem>
+            </>
+          )}
         </List>
       </nav>
     </header>
---
+++ frontend/context/AuthContext.tsx
@@ -0,0 +1,31 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { API_URL } from 'Config/index';
+
+const AuthContext = createContext();
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  //   register
+  const register = async (user) => {
+    console.log(user);
+  };
+  //   login
+  const login = async ({ email: identifier, password }) => {
+    console.log({ identifier, password });
+  };
+  //   logout
+  const logout = async () => {
+    console.log('logout');
+  };
+  //   check if user logged in
+  const checkUserLoggedIn = async (user) => {
+    console.log('Check');
+  };
+  return (
+    <AuthContext.Provider value={{ user, error, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export default AuthContext;
