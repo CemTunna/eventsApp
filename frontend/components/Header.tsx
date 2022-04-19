c2e54c9b0553b50eba06a5e805c2f88a054b655b
CemTunna <cemroot@gmail.com>
2022-04-19 23:33:20 +0300

    icons and links added

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem, Grid } from '@mui/material';
+import { List, ListItem, Grid, Button } from '@mui/material';
 import React, { useContext } from 'react';
 import styles from 'Styles/Header.module.css';
 import Link from 'next/link';
@@ -6,6 +6,8 @@ import Search from './Search';
 import AddIcon from '@mui/icons-material/Add';
 import LoginIcon from '@mui/icons-material/Login';
 import AuthContext from 'Context/AuthContext';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import LogoutIcon from '@mui/icons-material/Logout';
 const Header = () => {
   const { user, logout } = useContext(AuthContext);
   console.log('user', user);
@@ -34,15 +36,30 @@ const Header = () => {
                   </a>
                 </Link>
               </ListItem>
+              <ListItem>
+                <Link href='/account/dashboard'>
+                  <a className={styles.regular}>
+                    Dashboard
+                    <DashboardIcon style={{ color: '#fff', marginLeft: 5 }} />
+                  </a>
+                </Link>
+              </ListItem>
+              <ListItem>
+                <Button onClick={() => logout()}>
+                  {' '}
+                  <LogoutIcon style={{ color: '#fff', marginLeft: 5 }} />
+                  Logout
+                </Button>
+              </ListItem>
             </>
           ) : (
             <>
               <ListItem>
-                {/* <Link href='/account/login'>
+                <Link href='/account/login'>
                   <a className={styles.login}>
                     Login <LoginIcon style={{ color: '#fff', marginLeft: 5 }} />
                   </a>
-                </Link> */}
+                </Link>
               </ListItem>
             </>
           )}
