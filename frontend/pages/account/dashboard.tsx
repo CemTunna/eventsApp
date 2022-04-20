3c0c056fd75345cbf442cb0acb2613b75e55c54c
CemTunna <cemroot@gmail.com>
2022-04-20 12:59:35 +0300

    dashboard added

---
+++ frontend/pages/account/dashboard.tsx
@@ -0,0 +1,8 @@
+import Layout from 'Components/layout/Layout';
+import React from 'react';
+
+const DashboardPage = () => {
+  return <Layout title='Dashboard'>DashboardPage</Layout>;
+};
+
+export default DashboardPage;
