92bc68ad275bd25fd20d6c00ee477c10cfb97c69
CemTunna <cemroot@gmail.com>
2022-04-21 20:57:30 +0300

    events/me , cookies

--- backend/api/events/controllers/events.js
+++ backend/api/events/controllers/events.js
@@ -1,8 +1,18 @@
-'use strict';
-
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+module.exports = {
+  // get logged in users
+  async me(context) {
+    const user = context.state.user;
+    if (!user) {
+      return context.badRequest(null, [
+        { messages: [{ id: "No authorization header was found" }] },
+      ]);
+    }
+    const data = await strapi.services.events.find({ user: user.id });
+    if (!data) {
+      return context.notFound();
+    }
+    return sanitizeEntity(data, { model: strapi.models.events });
+  },
+};
