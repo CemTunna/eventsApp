2bfd6b7f68f1b002e8d2022be6a49dc5cfb1fa8b
CemTunna <cemroot@gmail.com>
2022-04-21 21:11:36 +0300

    dashboard

--- frontend/pages/account/dashboard.tsx
+++ frontend/pages/account/dashboard.tsx
@@ -1,8 +1,43 @@
 import Layout from 'Components/layout/Layout';
 import React from 'react';
-
-const DashboardPage = () => {
-  return <Layout title='Dashboard'>DashboardPage</Layout>;
+import { parseCookies } from 'Helpers/index';
+import { GetServerSideProps } from 'next';
+import { API_URL } from 'Config/index';
+import { Event } from 'typings';
+import { Grid } from '@mui/material';
+import Title from 'Components/Title';
+import SubTitle from 'Components/SubTitle';
+interface DashboardPageInterface {
+  events: Event[];
+}
+const DashboardPage: React.FC<DashboardPageInterface> = ({ events }) => {
+  console.log(events);
+  return (
+    <Layout title='Dashboard'>
+      <Grid>
+        <Title>Dashboard</Title>
+        <SubTitle>My events</SubTitle>
+        {events.map((item: Event) => (
+          <h3>{item.name}</h3>
+        ))}
+      </Grid>
+    </Layout>
+  );
 };
 
 export default DashboardPage;
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { token } = parseCookies(req);
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const events = await res.json();
+  return {
+    props: {
+      events,
+    },
+  };
+};
